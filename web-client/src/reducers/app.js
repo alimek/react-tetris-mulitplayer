1f301975bba723ac8bfee7a3df93db87137d959b
Grzegorz Mandziak <alimek@alimek.pl>
2018-03-03 11:41:40 +0100

    start app

---
+++ web-client/src/reducers/app.js
@@ -0,0 +1,11 @@
+const initialState = {
+  ready: false,
+  started: false,
+};
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
